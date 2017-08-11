8eed63fed8be33fca1deb02a35a8e4e73865302c
Kevin Smith <KevinH.Smith@coxinc.com>
2017-08-11 09:36:33 -0400

    Remove css template tag and rename target rendering modules

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import { Element } from './Element.js';
 
 export { Element };
 export { UI } from './UI.js';
-export { css } from './css.js';
 
-export { renderToDOM } from './render-dom.js';
-export { renderToString } from './render-string.js';
+export { renderToDOM } from './dom-target.js';
+export { renderToString } from './string-target.js';
 
 export function createElement(tag, props, children) {
   return new Element(tag, props, children);

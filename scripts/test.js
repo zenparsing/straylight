443e3317271394c90d2ab23ed0c1daa4d6da1470
zenparsing <zenparsing@gmail.com>
2024-11-22 15:53:03 -0500

    Bump dependencies

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,4 @@
 import { $ } from './exec.js';
 
-$('eslint -c scripts/.eslintrc.json src/*.js src/**/*.js test/*.js test/**/*.js');
+$('eslint -c scripts/eslint.config.js src/*.js src/**/*.js test/*.js test/**/*.js');
 $('node ./test/index.js');

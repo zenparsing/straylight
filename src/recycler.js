56e1aacaedc7da3c7ed002942526095d2edb60bc
zenparsing <zenparsing@gmail.com>
2025-11-23 21:09:00 -0500

    Use FinalizationRegistry to clean up recycler entries

--- src/recycler.js
+++ src/recycler.js
@@ -1,7 +1,12 @@
 let map = new Map();
 
+let registry = new FinalizationRegistry((key) => {
+  map.delete(key);
+});
+
 export function add(key, elem) {
   map.set(key, new WeakRef(elem));
+  registry.register(elem);
 }
 
 export function get(key, tag) {
